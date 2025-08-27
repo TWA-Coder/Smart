48ab9e5b73a88f35e0239004b45cdbdf879b11b0
TWA-Coder <twakaru744@gmail.com>
2025-08-27 16:57:10 +0200

    aug-27

--- Selenium/main.py
+++ Selenium/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -17,13 +19,42 @@ search_box2 = driver.find_element(By.ID,'lbl_country')
 search_box2.send_keys('rwanda')
 
 search_box2 = driver.find_element(By.ID,'lbl_username')
-search_box2.send_keys('')
+search_box2.send_keys('Mugabe Patrick')
 
 search_box2 = driver.find_element(By.ID,'lbl_password')
-search_box2.send_keys('')
+search_box2.send_keys('Mugab@39')
 
-next_button = driver.find_element(By.ID,'FormSubmit')
-next_button.click()
+next_button1 = driver.find_element(By.ID,'FormSubmit')
+next_button1.click()
+time.sleep(5)
+
+wait = WebDriverWait(driver, 10)
+
+try:
+
+    css_selector = f'[data-value = "0"]'
+    css_selector1 = f'[data-value = "accept"]'
+    search_box3 = wait.until(EC.visibility_of_element_located((By.ID, "typing")))
+    search_box3.click()
+    
+
+    search_box3 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector)))
+    search_box3.click()
+    search_box3.click()
+    search_box3.click()
+    search_box3.click()
+
+    search_box3 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector1)))
+    search_box3.click()
+
+except Exception as e:
+    print(f"Error: Element not found or not visible: {e}")
+
+time.sleep(5)
+
+next_button2 = driver.find_element(By.ID,'FormSubmit2')
+next_button2.click()
+
 
 
 time.sleep(60)
