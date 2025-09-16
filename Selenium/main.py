d68f432e00565a0a23b78179fee4fd3e99a32a20
TWA-Coder <twakaru744@gmail.com>
2025-09-16 08:47:04 +0200

    go

--- Selenium/main.py
+++ Selenium/main.py
@@ -8,7 +8,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from input_date import automate_daily_report_generation
+from selenium.webdriver.chrome.options import Options
+
+# The code below allows the user to login and download the scheme utilization report of the previous day once run!
+
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -65,6 +68,8 @@ next_button2.click()
 
 
 time.sleep(10)
+driver.maximize_window()
+time.sleep(10)
 
 # Here We start the process of downloading the report
 css_selector2 = f'[data-i18n="reports"]'
@@ -102,7 +107,7 @@ date_input = driver.find_element(By.CSS_SELECTOR, css_selector)
 date_input.click()
 time.sleep(10)
 DATE_INPUT_SELECTOR = f'[data-dateformat="yy-mm-dd"]'
-EMPTY_SELECTOR = f'[data-i18n="from"]'
+EMPTY_SELECTOR = f'[src="img/smartlogo.jpg"]'
 
 try:
     date_input_field = WebDriverWait(driver, 10).until(
@@ -110,17 +115,130 @@ try:
             )
     date_input_field.click()
     date_input_field.send_keys(yesterday_formatted)
-    EMPTY_SELECTOR.click()
+    #EMPTY_SELECTOR.click()
 
 except:
     print(f"An error occurred while processing {yesterday_formatted}")
-          
-    
+
+time.sleep(10)
+
+#Empty click 1
+
+empty_click1 = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH,"//label[@data-i18n='from']")))
+empty_click1.click()
+
+#Input to-date formmatted
+
+try:
+    date_input_field2 = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID,'txt_dateto'))
+            )
+    date_input_field2.click()
+    date_input_field2.send_keys(today_formatted)
+    #EMPTY_SELECTOR.click()
+
+except:
+    print(f"An error occurred while processing {today_formatted}")
+
+#empty click 2
+time.sleep(5)
+# empty_click2 = driver.find_element((By.XPATH, "//label[@data-i18n='to']"))
+# empty_click2.click()          
+# try:
+#     css_selector5 = f'[data-action="mnifyMenu"]'
+#     WebDriverWait(driver, 10).until(
+#                 EC.presence_of_element_located((By.CSS_SELECTOR,css_selector5))
+#             )
+#     search_box7 = driver.find_element(By.CSS_SELECTOR, css_selector5)
+#     search_box7.click()
+# except:
+#     print("css_selector5  is missing")
+
+
+#GENERATE_BUTTON_SELECTOR = driver.find_element(By.CLASS_NAME, 'btn.btn-primary.btn-sm')
+try:
+    WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[text()='Search']"))
+)
+    GENERATE_BUTTON= driver.find_element(By.XPATH, "//button[text()='Search']")
+    GENERATE_BUTTON.click()
+except:
+    print("GENERATE_BUTTON is missing")
+
+time.sleep(5)
 
 
 
+try:
+    EXPORT_BUTTON = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//button[text()='Export']"))
+)
+    EXPORT_BUTTON.click()
+except:
+    print("EXPORT_BUTTON  is missing")
 
+time.sleep(5)
+
+
+try:
+    EXPORT_TO_EXCEL = driver.find_element(By.XPATH,"//a[@data-i18n='exporttoexcel']")
+    EXPORT_TO_EXCEL.click()
+except:
+    print("EXPORT_TO_EXCEL  is missing")
+
+time.sleep(10)
+
+try:
+    CLICK_HERE = driver.find_element(By.XPATH,"//a[text()='click here']")
+    CLICK_HERE.click()
+except:
+    print("CLICK_HERE  is missing")
+
+time.sleep(10)
+try:
+    DOWNLOAD_LAST = driver.find_element(By.XPATH,"//a[@data-i18n='download']")
+    DOWNLOAD_LAST.click()
+except:
+    print("DOWNLOAD_LAST  is missing")
+
+try:
+    SEARCH_LAST = driver.find_element(By.XPATH,"//a[@data-i18n='search']")
+    SEARCH_LAST.click()
+    time.sleep(30)
+    SEARCH_LAST.click()
+    time.sleep(30)
+    SEARCH_LAST.click()
+except:
+    print("SEARCH_LAST  is missing")
 
 time.sleep(60)
+
+try:
+    FILE_LAST = driver.find_element(By.XPATH,"//a[contains(@href, 'download-report.jsp')]")
+    FILE_LAST.click()
+except:
+    print("FILE_LAST  is missing")
+
+time.sleep(3)
+
+try:
+    DOWNLOAD_POP = driver.find_element(By.XPATH,"//button[@id='download_btn']")
+    DOWNLOAD_POP.click()
+except:
+    print("DOWNLOAD_POP  is missing")
+
+time.sleep(10)
+
+try:
+    OPEN_FILE = driver.find_element(By.XPATH,"//button[@id='bot2-Msg1']")
+    OPEN_FILE.click()
+except:
+    print("OPEN_FILE  is missing")
+
+
+
+
+time.sleep(20)
 
 driver.quit()
